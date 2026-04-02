44aa3163dcb9fab0bd3eb57c1327833ff22287e9
rubenetiv <110274677+rubenetiv@users.noreply.github.com>
2026-04-01 22:20:00 -0600

    Add basic variable definitions and operations in Python

---
+++ variables.py
@@ -0,0 +1,32 @@
+# =========================
+# Ejercicio 1
+# =========================
+
+# Variables básicas
+nombre = "Juan"
+apellido = "Pérez"
+nombre_completo = nombre + " " + apellido
+pais = "México"
+ciudad = "CDMX"
+edad = 25
+anio = 2026
+is_married = False
+is_true = True
+is_light_on = True
+
+# Longitud del nombre
+print("Longitud del nombre:", len(nombre))
+
+# Operaciones
+num_one = 5
+num_two = 4
+
+total = num_one + num_two
+diff = num_one - num_two
+producto = num_one * num_two
+division = num_one / num_two
+resto = num_two % num_one
+exp = num_one ** num_two
+floor_division = num_one // num_two
+
+print(total, diff, producto, division, resto, exp, floor_division)
